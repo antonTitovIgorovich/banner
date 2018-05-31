f97b146e574e54fb6f74549f7f3c2d8001557cee
AT <mail@mail.com>
2018-05-31 19:25:28 +0200

    Changed LoginController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@index')->name('home');
 
@@ -9,3 +10,8 @@ Auth::routes();
 Route::get('/verify/{token}', 'Auth\RegisterController@verify')->name('register.verify');
 
 Route::get('/cabinet', 'Cabinet\HomeController@index')->name('cabinet');
+
+Route::middleware(['auth'])->group(function (){
+    Route::get('/admin', 'Admin\HomeController@index')->name('admin.home');
+});
+
