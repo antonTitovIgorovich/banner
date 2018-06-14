346ebd86ee01ad02976a21f036a8d4866d159131
AT <mail@mail.com>
2018-06-14 19:48:47 +0200

    Created region migration

---
+++ database/factories/RegionFactory.php
@@ -0,0 +1,11 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Entity\Region::class, function (Faker $faker) {
+    return [
+        'name' => $name = $faker->unique()->city,
+        'slug' => str_slug($name),
+        'parent_id' => null
+    ];
+});
